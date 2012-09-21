40dbbe5e941f91f7ef4d0e00936be661d0b035ed
Thomas Skovgaard Gielfeldt <thomas@gielfeldt.com>
2012-09-21 20:13:00 +0200

    Allow deep-link to filters.

--- js/ultimate_cron.js
+++ js/ultimate_cron.js
@@ -1,33 +1,30 @@
 jQuery(document).ready(function($) {
   $("a#ultimate-cron-show-all").click(function() {
     $(".ultimate-cron-admin-status").parent().show();
-    return false;
   });
   $("a#ultimate-cron-show-error").click(function() {
     $("tr .ultimate-cron-admin-status:not(.ultimate-cron-admin-status-error)").parent().hide();
     $("tr .ultimate-cron-admin-status-error").parent().show();
-    return false;
   });
   $("a#ultimate-cron-show-warning").click(function() {
     $("tr .ultimate-cron-admin-status:not(.ultimate-cron-admin-status-warning)").parent().hide();
     $("tr .ultimate-cron-admin-status-warning").parent().show();
-    return false;
   });
   $("a#ultimate-cron-show-info").click(function() {
     $("tr .ultimate-cron-admin-status:not(.ultimate-cron-admin-status-info)").parent().hide();
     $("tr .ultimate-cron-admin-status-info").parent().show();
-    return false;
   });
   $("a#ultimate-cron-show-success").click(function() {
     $("tr .ultimate-cron-admin-status:not(.ultimate-cron-admin-status-success)").parent().hide();
     $("tr .ultimate-cron-admin-status-success").parent().show();
-    return false;
   });
   $("a#ultimate-cron-show-running").click(function() {
     $("tr .ultimate-cron-admin-status:not(.ultimate-cron-admin-status-running)").parent().hide();
     $("tr .ultimate-cron-admin-status-running").parent().show();
-    return false;
   });
 
   $("#ultimate-cron-view").tablesorter();
+
+  var sel = location.hash.substring(1);
+  $('a#ultimate-cron-' + sel).trigger('click');
 });
