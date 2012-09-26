39f91f1d627532930c32a498bba49295c6a2d259
Thomas Skovgaard Gielfeldt <thomas@gielfeldt.com>
2012-09-26 10:09:32 +0200

    Chunk logic in launcher. Check rules before cron job trigger to avoid potentially unnecessary http request. Added initial js settings from Drupal. Fixed log message formatting.

--- js/nodejs.ultimate_cron.js
+++ js/nodejs.ultimate_cron.js
@@ -2,42 +2,42 @@
 
 
 $(function() {
-  Drupal.settings.ultimate_cron = Drupal.settings.ultimate_cron || {};
-  Drupal.settings.ultimate_cron.processes = Drupal.settings.ultimate_cron.processes || {};
-  Drupal.settings.ultimate_cron.skew = 0;
-  Drupal.settings.ultimate_cron.secondCounterStart = (new Date()).getTime() / 1000;
-  Drupal.settings.ultimate_cron.secondCounter = 0;
-
   // Ping Drupal for initial status of processes.
   $.ajax({
     type: 'GET',
     url: '/admin/ultimate-cron/service/process-status',
     data: '',
     dataType: 'json',
-    success: function (processes) {
+    success: function (result) {
+      // Drupal.settings.ultimate_cron.initial_processes = result.processes;
+      // Drupal.settings.ultimate_cron.processes = result.processes;
+      // setTimeout(&ultimateCronInitialProcess, 100);
     }
   });
 
+/**/
   // Setup progress counter
   setInterval(function() {
     var time = (new Date()).getTime() / 1000;
-    Drupal.settings.ultimate_cron.secondCounter = Math.round(time - Drupal.settings.ultimate_cron.secondCounterStart);
     $.each(Drupal.settings.ultimate_cron.processes, function (name, process) {
+      // console.log(name + ' - ' + process.exec_status);
       if (process.exec_status == 2) {
-      var row = 'row-' + name;
-      var seconds = Math.round(((new Date()).getTime() / 1000) - process.start_time - Drupal.settings.ultimate_cron.skew);
-      seconds = seconds < 0 ? 0 : seconds;
-      var formatted = (new Date(seconds * 1000)).toISOString().substring(11, 19);
-      if (process.progress >= 0) {
-        var progress = Math.round(process.progress * 100);
-        formatted += ' (' + progress + '%)';
-      }
-      $('tr.' + row + ' td.ultimate-cron-admin-status-running').closest('tr.' + row).find('td.ultimate-cron-admin-end').html(formatted);
+        var row = 'row-' + name;
+        var seconds = Math.round((time - Drupal.settings.ultimate_cron.skew) - process.start_time);
+        seconds = seconds < 0 ? 0 : seconds;
+        var formatted = (new Date(seconds * 1000)).toISOString().substring(11, 19);
+        if (process.progress >= 0) {
+          var progress = Math.round(process.progress * 100);
+          formatted += ' (' + progress + '%)';
+        }
+        // console.log(formatted);
+        $('tr.' + row + ' td.ultimate-cron-admin-status-running').closest('tr.' + row).find('td.ultimate-cron-admin-end').html(formatted);
       }
     });
   }, 1000);
-});
+/**/
 
+});
 
 Drupal.Nodejs.callbacks.nodejsBackgroundProcess = {
   updateSkew: function (time) {
@@ -47,30 +47,29 @@ Drupal.Nodejs.callbacks.nodejsBackgroundProcess = {
 
   callback: function (message) {
     var action = message.data.action;
+    if (
+      // action != 'locked' &&
+      action != 'claimed' &&
+      action != 'dispatch' &&
+      // action != 'remove' &&
+      action != 'setProgress' &&
+      action != 'ultimateCronStatus'
+    ) {
+      return;
+    }
+
     var process = message.data.background_process;
     var ultimate_cron = message.data.ultimate_cron;
     var name = process.handle.replace(/^uc:/, '');
     if (name == process.handle) {
       return;
     }
+    console.log(name + ' ' + action + ' - ' + process.exec_status);
     Drupal.settings.ultimate_cron.processes[name] = process;
 
     var row = 'row-' + name;
 
     switch (action) {
-      case 'locked':
-        this.updateSkew(message.data.timestamp);
-        $('tr.' + row + ' td.ultimate-cron-admin-status').attr('class', 'ultimate-cron-admin-status ultimate-cron-admin-status-running');
-        $('tr.' + row + ' td.ultimate-cron-admin-start').html(ultimate_cron.start_time);
-        $('tr.' + row + ' td.ultimate-cron-admin-end').html(Drupal.t('Starting'));
-        $('tr.' + row + ' td.ultimate-cron-admin-execute').attr('title', Drupal.t('Unlock'));
-        $('tr.' + row + ' td.ultimate-cron-admin-status').html('<span>' + Drupal.t('Starting') + '</span>');
-        $('tr.' + row + ' td.ultimate-cron-admin-status').attr('title', Drupal.t('Running on !service_host', {'!service_host': process.service_host ? process.service_host : Drupal.t('N/A')}));
-        var link = $('tr.' + row + ' td.ultimate-cron-admin-execute a');
-        link.attr('href', ultimate_cron.unlockURL);
-        link.html('<span>' + Drupal.t('Unlock') + '</span>');
-        $('tr.' + row + ' td.ultimate-cron-admin-execute').attr('class', 'ultimate-cron-admin-unlock');
-        break;
       case 'dispatch':
         this.updateSkew(message.data.timestamp);
         $('tr.' + row + ' td.ultimate-cron-admin-status').attr('class', 'ultimate-cron-admin-status ultimate-cron-admin-status-running');
@@ -95,6 +94,7 @@ Drupal.Nodejs.callbacks.nodejsBackgroundProcess = {
         $('tr.' + row + ' td.ultimate-cron-admin-execute').attr('class', 'ultimate-cron-admin-unlock');
         break;
       case 'setProgress':
+        break;
         this.updateSkew(message.data.timestamp);
         $('tr.' + row + ' td.ultimate-cron-admin-status').attr('class', 'ultimate-cron-admin-status ultimate-cron-admin-status-running');
         $('tr.' + row + ' td.ultimate-cron-admin-start').html(ultimate_cron.start_time);
@@ -116,10 +116,6 @@ Drupal.Nodejs.callbacks.nodejsBackgroundProcess = {
             return this.callback(message);
         }
         return;
-      case 'remove':
-        this.updateSkew(message.data.timestamp);
-        delete Drupal.settings.ultimate_cron.processes[log.name];
-        return;
       default:
         return;
     }
@@ -136,11 +132,12 @@ Drupal.Nodejs.callbacks.nodejsUltimateCron = {
     switch (action) {
       case 'log':
         var log = message.data.log;
+        // console.log(log.name + ' shutdown');
         delete Drupal.settings.ultimate_cron.processes[log.name];
         var row = 'row-' + log.name;
         $('tr.' + row + ' td.ultimate-cron-admin-status').attr('class', 'ultimate-cron-admin-status ultimate-cron-admin-status-' + log.formatted.severity);
         $('tr.' + row + ' td.ultimate-cron-admin-status').html('<span>' + log.severity + '</span>');
-        $('tr.' + row + ' td.ultimate-cron-admin-status').attr('title', log.msg ? log.msg : Drupal.t('No errors'));
+        $('tr.' + row + ' td.ultimate-cron-admin-status').attr('title', log.formatted.msg ? log.formatted.msg : Drupal.t('No errors'));
         $('tr.' + row + ' td.ultimate-cron-admin-start').html(log.formatted.start_time);
         $('tr.' + row + ' td.ultimate-cron-admin-start').attr('title', Drupal.t('Previous run started @ !timestamp', {'!timestamp': log.formatted.start_time}));
         $('tr.' + row + ' td.ultimate-cron-admin-end').html(log.formatted.duration);
@@ -153,6 +150,7 @@ Drupal.Nodejs.callbacks.nodejsUltimateCron = {
       default:
         return;
     }
+
     var sel = location.hash.substring(1);
     sel = sel ? sel : 'show-all';
     $('a#ultimate-cron-' + sel).trigger('click');
